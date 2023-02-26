9e5ad27f4e9284886caad4baf312c17138f4dbbe
Ildar Vagapov <Ildar-Sarbai@Rambler.ru>
2023-02-26 15:01:40 +0300

    написана программа

---
+++ 2zadachaMassivChet/Program.cs
@@ -0,0 +1,64 @@
+﻿// **Задача 49:** Задайте двумерный массив.
+//  Найдите элементы, у которых оба индекса чётные,
+//   и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 2 3 4
+
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+// 1 **4** 3 **16**
+
+
+
+int m = 4, n = 4;
+int[,] mas = GetArray(m,n);
+PrintArray(mas);
+int[,] arra = ChangetArray(mas);
+System.Console.WriteLine();
+PrintArray(arra);
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i+j;
+        }
+    }
+    return result;
+}
+
+int[,] ChangetArray(int[,] array)
+{
+        for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i > 0 && i % 2 == 1 && j > 0 && j % 2 == 1)
+            array[i,j] *=array[i,j];
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
